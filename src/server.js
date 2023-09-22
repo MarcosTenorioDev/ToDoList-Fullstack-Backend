c99e374662440a2640df7123f7ab76630bf740bc
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2023-09-22 00:03:07 -0300

    :bug: trying fix some issues

--- src/server.js
+++ src/server.js
@@ -9,6 +9,13 @@ app.use(express.json());
 
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5174');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use(todoRoutes)
 
 app.get("/health", (req, res) => {
