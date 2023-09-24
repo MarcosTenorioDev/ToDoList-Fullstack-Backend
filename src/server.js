efa8d62dff34f7378a57f860e8ba9ff571ec8c05
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2023-09-24 09:52:33 -0300

    app.use((req, res, next) => {

--- src/server.js
+++ src/server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5174');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(todoRoutes)
 
